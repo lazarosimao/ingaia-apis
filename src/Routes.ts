c7863670fdac3ed3ecf8b59cd07adef009b5b07a
Lázaro Simão <lssimao.infor@gmail.com>
2020-12-18 11:56:34 -0300

    change values router version

--- src/Routes.ts
+++ src/Routes.ts
@@ -9,7 +9,7 @@ class Routes {
   }
 
   getRoutes(app: Application): void {
-    app.route('/api/v1/version').get((req: Request, res: Response) => res.send('Version: 1.0.0'));
+    app.route('/api/v1/version').get((req: Request, res: Response) => res.json({ version: "1.0.0", developer: "Lázaro Simão" }));
     app.route('/api/v1/square-meter-value').get((req: Request, res: Response) => squareMeterValueController.handle(req, res));
     app.route('/api/v1/property-value').post((req: Request, res: Response) => propertyValueController.handle(req, res));
   }
